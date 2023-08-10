1d4daac029483792d1c31bc4a608f3a3e08e3610
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2023-08-10 20:05:52 +0100

    Bump version number

--- src/topsy/__init__.py
+++ src/topsy/__init__.py
@@ -1,6 +1,6 @@
 """topsy - An astrophysics simulation visualization package based on OpenGL, using pynbody for reading data"""
 
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 import logging
 import sys
