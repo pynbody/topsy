25c1ef5542e6d38054d836b7be13480883346129
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2023-09-24 20:59:18 +0100

    New minor version for GUI work

--- src/topsy/__init__.py
+++ src/topsy/__init__.py
@@ -1,6 +1,6 @@
 """topsy - An astrophysics simulation visualization package based on webgpu, using pynbody for reading data"""
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 import logging
 import sys
