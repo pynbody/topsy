9ba913288050adbf53f3087a0248680e10382fba
Andrew Pontzen <andrew.p.pontzen@durham.ac.uk>
2026-06-16 08:30:10 +0100

    Fix for latest versions of wgpu/rendercanvas

--- src/topsy/visualizer.py
+++ src/topsy/visualizer.py
@@ -162,8 +162,7 @@ class VisualizerBase:
             if max_buffer_size > 2**63:
                 max_buffer_size = 2**63
             type(self).device: wgpu.GPUDevice = self.adapter.request_device_sync(
-                required_features=["texture-adapter-specific-format-features", "float32-filterable",
-                                  "multi-draw-indirect"],
+                required_features=["texture-adapter-specific-format-features", "float32-filterable"],
                 required_limits={"max_buffer_size": max_buffer_size})
         self.context: wgpu.GPUCanvasContext = self.canvas.get_context("wgpu")
 
